d618cb7779f104edcdf601df82db4e22ef280282
kiorky <kiorky@cryptelium.net>
2012-01-30 09:14:48 +0100

    complete genericsetup infra

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     namespace_packages=[         'collective',
          'collective.externalimageeditor',],
     version=version,
-    description='Project collective.externalimageeditor externalimageeditor product',
+    description='Project %s externalimageeditor product',
     long_description=long_description,
     classifiers=classifiers,
     keywords='plone image editor',
